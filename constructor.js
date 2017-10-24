4823729a354c5cf978a34e965427cb2d070a74a6
kimberly owens <kimberlyowens@lawn-128-61-47-145.lawn.gatech.edu>
2017-10-24 18:22:55 -0400

    got the inquirer thing to work

--- constructor.js
+++ constructor.js
@@ -1,12 +1,22 @@
 var newGame = require("./newGame.js");
 var letterSelection = require("./letter");
+var inquirer = require('inquirer');
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'letter',
+        message: 'Guess a letter!'
+    }
+]).then(function(user){
+    newGame();
+    letterSelection();
+    }
+);
 
 
 
 
-
-newGame();
-letterSelection();
 
 
 // console.log(dashesAndCorrectGuesses.push("_"))
