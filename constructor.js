794b7b268ebba4999d77629472fd16c67d9cca44
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-11-02 17:00:32 -0400

    building a new letter function

--- constructor.js
+++ constructor.js
@@ -7,24 +7,32 @@ var breakfastFoods = ["croissants", "bagels", "oatmeal","eggs","fruit","coffee",
 
 let nodeWord = new Word(breakfastFoods[Math.floor(Math.random() * breakfastFoods.length)]);
 console.log(nodeWord)
-// let guess = newWord.getLetterIndex(process.argv[2]);
+let dashes = nodeWord.dashesArray;
+let word = nodeWord.foodArray;
 
-// console.log(nodeWord);
 
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'letter',
+        message: 'Guess a letter!'
 
-// inquirer.prompt([
-//     {
-//         type: 'input',
-//         name: 'letter',
-//         message: 'Guess a letter!'
-//
-//     }
-//
-// ]).then(function (user) {
-//
-//
-//
-// });
+    }
+
+]).then(function (user) {
+
+    if (word[i] = user.letter) {
+for ( i = 0; i < word.length, i ++)
+
+ {
+        dashes = word.replace(user.letter);
+        console.log('here', dashes);
+    }
+     }
+
+let test = new Correct(user.letter);
+
+});
 
 
 // newWord.getLetterIndex(guess)
